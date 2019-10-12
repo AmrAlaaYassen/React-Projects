897b6577f1941081c72868dd36a83eabd97189ca
Amr Alaa Yassen <amralaa566@gmail.com>
2019-10-12 17:38:36 +0200

    Contact Keeper - Finish Contacts Routes and APIs

--- Contact-keeper/routes/contacts.js
+++ Contact-keeper/routes/contacts.js
@@ -1,20 +1,64 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
-router.get("/", (req, res) => {
-  res.send("get all contacts");
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error.');
+  }
 });
 
-router.post("/", (req, res) => {
-  res.send("Add contact");
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+        check('email', 'email is required').not().isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+
+   
+    try {
+      let contact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+      contact = await contact.save()
+      res.send(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+router.put('/:id', (req, res) => {
+  res.send('Update contact');
 });
 
-router.put("/:id", (req, res) => {
-  res.send("Update contact");
-});
-
-router.delete("/:id", (req, res) => {
-  res.send("delete contact");
+router.delete('/:id', (req, res) => {
+  res.send('delete contact');
 });
 
 module.exports = router;
