486f1b9b7ffcb9dc9e62aeed8af3e7ce1659cbf7
Amr Alaa Yassen <amralaa566@gmail.com>
2019-10-12 17:16:56 +0200

    Contact Keeper - Add Auth Module add register function add login function add get user info function

--- Contact-keeper/server.js
+++ Contact-keeper/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const connectDB = require('./config/db');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Connect DB
+connectDB();
 
+//Init Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
