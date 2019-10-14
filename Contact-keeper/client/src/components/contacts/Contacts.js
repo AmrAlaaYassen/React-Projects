167d5b12420a3f2ac3283f1761e39639d8bd6f22
Amr Alaa Yassen <amralaa566@gmail.com>
2019-10-14 15:27:03 +0200

    Contact Keeper - Add Contacts & ContactItem components

---
+++ Contact-keeper/client/src/components/contacts/Contacts.js
@@ -0,0 +1,15 @@
+import React, {Fragment, useContext} from 'react'
+import ContactItem from './ContactItem';
+import ContactContext from '../../context/contacts/contactContext';
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+  const {contacts} = contactContext;
+  return (
+    <div>
+      {contacts.map(contact => (
+         <ContactItem key = {contact.id} contact={contact} />
+      ))}    </div>
+  )
+}
+
+export default Contacts
