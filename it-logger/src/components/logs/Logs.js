956eb9edf91a024ef0c207b2ed24c4b45b080cfd
Amr Alaa Yassen <amralaa566@gmail.com>
2019-10-17 00:02:33 +0200

    IT Logger - convert getting logs state management to redux

--- it-logger/src/components/logs/Logs.js
+++ it-logger/src/components/logs/Logs.js
@@ -1,41 +1,42 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import Preloader from '../layouts/Preloader';
-import LogItem from './LogItem'
-const Logs = () => {
-
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const getLogs = async () =>  {
-    setLoading(true);
-    
-    const res = await fetch('/logs')
-    const logs = await res.json();
-
-    setLogs(logs);
-    setLoading(false)
-  }
-
+import LogItem from './LogItem';
+import PropTypes from 'prop-types';
+import { getLogs } from '../../actions/logActions';
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
     //eslint-disable-next-line
   }, []);
 
-  if (loading) { 
-    return <Preloader />
+  if (loading || logs === null) {
+    return <Preloader />;
   }
   return (
-    <ul className="collection with-header">
-      <li className="collection-header">
-        <h4 className="center">System Logs</h4>
+    <ul className='collection with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
       </li>
 
-
-      {!loading && logs.length === 0? (<h4>No Logs...</h4>): logs.map(log => (
-        <LogItem log={log} key={log.id}/>
-      ))}
+      {!loading && logs.length === 0 ? (
+        <h4>No Logs...</h4>
+      ) : (
+        logs.map(log => <LogItem log={log} key={log.id} />)
+      )}
     </ul>
-  )
-}
+  );
+};
 
-export default Logs
+Logs.propTypes = {
+  log: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  log: state.log
+});
+
+export default connect(
+  mapStateToProps,
+  { getLogs }
+)(Logs);
