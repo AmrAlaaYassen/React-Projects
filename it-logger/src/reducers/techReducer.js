c3ab6cd1a5c182bee5743312bcec58b7b16c1ff5
Amr Alaa Yassen <amralaa566@gmail.com>
2019-10-17 13:31:03 +0200

    IT Logger - Add Delete Technician Functionality

--- it-logger/src/reducers/techReducer.js
+++ it-logger/src/reducers/techReducer.js
@@ -14,28 +14,32 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-
     case GET_TECHS:
-      return{
+      return {
         ...state,
         techs: action.payload,
         loading: false
-      }
+      };
     case ADD_TECH:
-      return { 
+      return {
         ...state,
         techs: [...state.techs, action.payload]
-      }
+      };
+    case DELETE_TECH:
+      return {
+        ...state,
+        techs: state.techs.filter(tech => tech.id !== action.payload)
+      };
     case SET_LOADING:
       return {
         ...state,
         loading: true
       };
     case TECHS_ERROR:
-      return { 
+      return {
         ...state,
         error: action.payload
-      }
+      };
     default:
       return state;
   }
